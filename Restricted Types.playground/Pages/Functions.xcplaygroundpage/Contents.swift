1181a4d3cfab2cf6909b9db0725c44e11aff4a35
Sebastian Grail <sgrail@gmail.com>
2016-01-10 20:25:52 +1100

    Draft functions

---
+++ Restricted Types.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,113 @@
+protocol Predicate {
+    typealias Argument
+    
+    static func isValid (value: Argument) -> Bool
+}
+
+struct Subset <P: Predicate> {
+    let value: P.Argument
+    
+    init? (value: P.Argument) {
+        guard P.isValid(value) else {
+            return nil
+        }
+        self.value = value
+    }
+}
+
+struct And <A: Predicate, B: Predicate where A.Argument == B.Argument>: Predicate {
+    static func isValid(value: A.Argument) -> Bool {
+        return A.isValid(value) && B.isValid(value)
+    }
+}
+
+
+struct Or <A: Predicate, B: Predicate where A.Argument == B.Argument>: Predicate {
+    static func isValid(value: A.Argument) -> Bool {
+        return A.isValid(value) || B.isValid(value)
+    }
+}
+
+protocol StaticValue {
+    typealias Value
+    
+    static var value: Value { get }
+}
+
+struct DoubleZero: StaticValue {
+    static var value: Double { return 0 }
+}
+
+struct DoubleOne: StaticValue {
+    static var value: Double { return 1 }
+}
+
+struct GreaterThan <V: StaticValue where V.Value: Comparable> : Predicate {
+    static func isValid(value: V.Value) -> Bool {
+        return value > V.value
+    }
+}
+
+struct LesserThan <V: StaticValue where V.Value: Comparable> : Predicate {
+    static func isValid(value: V.Value) -> Bool {
+        return value < V.value
+    }
+}
+
+struct EqualTo <V: StaticValue where V.Value: Equatable> : Predicate {
+    static func isValid(value: V.Value) -> Bool {
+        return value == V.value
+    }
+}
+
+
+protocol Function {
+    typealias In
+    typealias Out
+    
+    static func apply (value: In) -> Out
+}
+
+struct Characters: Function {
+    static func apply (value: String) -> String.CharacterView {
+        return value.characters
+    }
+}
+
+struct Count <C: CollectionType>: Function {
+    static func apply (value: C) -> C.Index.Distance {
+        return value.count
+    }
+}
+
+extension GreaterThan: Function {
+    static func apply(value: V.Value) -> Bool {
+        return self.isValid(value)
+    }
+}
+
+struct Compose <B: Function, A: Function where A.Out == B.In>: Function {
+    static func apply(value: A.In) -> B.Out {
+        return B.apply(A.apply(value))
+    }
+}
+
+struct Zero: StaticValue {
+    static var value: Int { return 0 }
+}
+
+typealias StringLength = Compose<Count<String.CharacterView>, Characters>
+
+let x = StringLength.apply("asdf")
+
+struct FunctionPredicate <F: Function where F.Out == Bool>: Predicate {
+    static func isValid(value: F.In) -> Bool {
+        return true
+    }
+}
+
+//struct Add <A: StaticValue, B: StaticValue where A.Value == Double, B.Value == Double>: Function {
+//
+//}
+
+
