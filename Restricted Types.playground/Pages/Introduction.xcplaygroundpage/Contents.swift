e9eba9ccc8b78bf2caa4cfdf262b1ba3e556b8fa
Sebastian Grail <sgrail@gmail.com>
2016-01-14 18:12:07 +1100

    Add intrudction and TOC

---
+++ Restricted Types.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*:
+
+# Subset types
+
+## Introduction
+
+Swift has built in mechanisms to create product and sum types.
+This playground explores a possible implementation of subset types using Swift's type system.
+
+
+## Table of Contents
+
+1. [Swift's Type Safety](Swift's%20Type%20Safety)
+2. [Baby's First Subsets](Baby's%20First%20Subsets)
+3. [Generic Subset Type](Generic%20Subset%20Type)
+4. [Composing Predicates](Composing%20Predicates)
+5. [Compile Time Values](Compile%20Time%20Values)
+6. [Functions](Functions)
+
+*/
